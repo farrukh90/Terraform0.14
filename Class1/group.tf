dfb7002592759aa187c39a3c35f1122f2de2d489
farrukh90 <farrukhsadykov@gmail.com>
2021-02-13 18:27:09 +0000

    added group

--- Class1/group.tf
+++ Class1/group.tf
@@ -1,3 +1,11 @@
 resource "aws_iam_group" "developers2" {
     name = "developers2"
 }
+
+resource "aws_iam_group_membership" "developers_team" {
+  name = "developers-group-membership"
+  users = [
+    aws_iam_user.tim.name
+  ]
+  group = aws_iam_group.developers.name
+}
