a80f1b94d8989915a1c8b453ee066b3837bebed1
farrukh90 <farrukhsadykov@gmail.com>
2021-02-16 01:50:21 +0000

    added ebs

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "web" {
   ami           = "ami-02354e95b39ca8dec"
   instance_type = "t3.micro"
   key_name = aws_key_pair.deployer2.key_name
-  count = 5
   associate_public_ip_address = true
   availability_zone  = "us-east-1a"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
@@ -11,3 +10,4 @@ resource "aws_instance" "web" {
     "Name" = "Farrukh"
   }
 }
+
