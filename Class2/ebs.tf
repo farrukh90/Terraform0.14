4b92f4c5e673c6e27810dc9e0ce1844470276f59
farrukh90 <farrukhsadykov@gmail.com>
2021-02-16 02:30:51 +0000

    added ec2 with import

--- Class2/ebs.tf
+++ Class2/ebs.tf
@@ -9,3 +9,16 @@ resource "aws_volume_attachment" "ebs_att" {
   volume_id   = aws_ebs_volume.example.id
   instance_id = aws_instance.web.id
 }
+
+
+resource "aws_ebs_volume" "example2" {
+  availability_zone = "us-east-1a"
+  size              = 100
+}
+
+
+resource "aws_volume_attachment" "ebs_att2" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example2.id
+  instance_id = aws_instance.web2.id
+}
--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -11,3 +11,16 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "aws_instance" "web2" {
+  ami                         = "ami-02354e95b39ca8dec"
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.deployer2.key_name
+  associate_public_ip_address = true
+  availability_zone           = "us-east-1a"
+  vpc_security_group_ids      = [aws_security_group.allow_tls.id]
+  user_data                   = file("userdata.sh")
+  tags = {
+    "Name" = "Farrukh"
+    "CreatedBy" = "terraform"
+  }
+}
