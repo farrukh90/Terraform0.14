f770e5f67b72416fc210885d0154ec8448623f0a
farrukh90 <farrukhsadykov@gmail.com>
2021-02-16 01:13:01 +0000

    added ec2 with userdata

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -3,4 +3,13 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
   key_name = aws_key_pair.deployer2.key_name
   count = 5
+  associate_public_ip_address = true
+  availability_zone  = "us-east-1a"
+  user_data = file("userdata.sh")
+  
+  
+  
+  tags = {
+    "Name" = "Farrukh"
+  }
 }
