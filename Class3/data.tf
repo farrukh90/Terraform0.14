6654560f808e57f08444bcdf18923f054273fb9d
farrukh90 <farrukhsadykov@gmail.com>
2021-02-18 01:56:44 +0000

    added it

--- Class3/data.tf
+++ Class3/data.tf
@@ -48,16 +48,4 @@ resource "aws_instance" "web" {
     ami = data.aws_ami.centos.id
     instance_type = "t2.micro"
     key_name = aws_key_pair.deployer3.key_name
-    provisioner "remote-exec" {
-        inline  = [
-            "sudo yum install httpd -y",
-            "sudo systemctl start httpd"
-        ]
-    connection {
-        type     = "ssh"
-        user     = "centos"
-        private_key = file("~/.ssh/id_rsa")
-        host     = aws_instance.web.public_ip
-    }
-  }
 }
