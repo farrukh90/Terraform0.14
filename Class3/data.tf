1eee20b17c202d2c4766aa8043ab0805cfd2ce67
farrukh90 <farrukhsadykov@gmail.com>
2021-02-18 00:39:53 +0000

    added centos

--- Class3/data.tf
+++ Class3/data.tf
@@ -1,19 +1,46 @@
+# This line pulls Ubuntu AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
-
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["099720109477"] # Canonical
 }
 
+
+# Shows AMI ID
 output "UBUNTU_ID" {
   value = data.aws_ami.ubuntu.id
 }
+
+
+
+# This line pulls CentOS AMI 
+data "aws_ami" "centos" {
+  most_recent = true
+  owners = ["679593333241"] # CentOS Owner
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM *"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Shows AMI ID
+output "CENTOS_ID" {
+  value = data.aws_ami.centos.id
+}
+
+
+
+
+
+
