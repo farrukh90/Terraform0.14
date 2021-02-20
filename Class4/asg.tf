67eb4caaad27c4fe5303194c78faf5ea89da0759
farrukh90 <farrukhsadykov@gmail.com>
2021-02-20 17:44:28 +0000

    added template

--- Class4/asg.tf
+++ Class4/asg.tf
@@ -1,13 +1,21 @@
-resource "aws_autoscaling_group" "bar" {
-  name                 = "terraform-asg-example"
-  launch_configuration = aws_launch_configuration.as_conf.name
-  availability_zones   = data.aws_availability_zones.all.names
-  min_size             = 1
-  max_size             = 2
-  lifecycle {
-    create_before_destroy = true
+resource "aws_autoscaling_group" "example" {
+  availability_zones = data.aws_availability_zones.all.names
+  desired_capacity = 1
+  max_size         = 1
+  min_size         = 1
+  mixed_instances_policy {
+    launch_template {
+      launch_template_specification {
+        launch_template_id = aws_launch_template.example.id
+      }
+      override {
+        instance_type     = "c4.large"
+        weighted_capacity = "3"
+      }
+      override {
+        instance_type     = "c3.large"
+        weighted_capacity = "2"
+      }
+    }
   }
 }
-
-
-
--- Class4/lc.tf
+++ Class4/lc.tf
@@ -1,7 +1,5 @@
-resource "aws_launch_configuration" "as_conf" {
-  name          = "web_config"
+resource "aws_launch_template" "example" {
+  name_prefix   = "example"
   image_id      = data.aws_ami.image.id
   instance_type = "t2.micro"
-  user_data = file("userdata.sh")
-  spot_price = "0.5"
 }
