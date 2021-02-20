1b563e6dcfc97b9b90a4d21c55b52b274d217bde
farrukh90 <farrukhsadykov@gmail.com>
2021-02-20 18:57:05 +0000

    added gitignore

---
+++ Class4/sec_group.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "asg-sec-group" {
+  name        = "asg-sec-group${var.app_name}"
+  description = "Allow TLS inbound traffic"
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.ssh_cidr_blocks
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
