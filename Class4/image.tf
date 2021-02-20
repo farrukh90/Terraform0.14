0a890af3893799c1a8b36e8be41beac000ece0d7
farrukh90 <farrukhsadykov@gmail.com>
2021-02-20 16:47:01 +0000

    added new ami

--- Class4/image.tf
+++ Class4/image.tf
@@ -1,4 +1,15 @@
 data "aws_ami" "image" {
   most_recent = true
-  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
