29a1cc805be490185ac66c01d95fe64e91ddbcf9
farrukh90 <farrukhsadykov@gmail.com>
2021-02-20 19:26:50 +0000

    added userdata

--- Class4/lc.tf
+++ Class4/lc.tf
@@ -3,4 +3,6 @@ resource "aws_launch_template" "example" {
   image_id      = data.aws_ami.image.id
   instance_type = "t2.micro"
   key_name      = aws_key_pair.asg-key-pair.key_name
+  user_data = filebase64("${path.module}/userdata.sh")
+
 }
