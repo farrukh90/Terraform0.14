4704761d950b1acca7a6faba4500bc64e9fe949e
farrukh90 <farrukhsadykov@gmail.com>
2021-02-23 02:11:09 +0000

    added 	depends_on = [ aws_internet_gateway.gw ]

--- Class5/private_subnet.tf
+++ Class5/private_subnet.tf
@@ -3,6 +3,7 @@ resource "aws_eip" "nat" {
 	tags = var.tags
 }
 resource "aws_nat_gateway" "gw" {
+	depends_on = [ aws_internet_gateway.gw ]
 	allocation_id = aws_eip.nat.id
 	subnet_id = aws_subnet.public1.id
 	tags = var.tags
