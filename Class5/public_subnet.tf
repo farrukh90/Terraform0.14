2915ce404a0b8737a9ca2a8824d6c1af92ad494b
farrukh90 <farrukhsadykov@gmail.com>
2021-02-23 00:55:50 +0000

    added public subnet association

--- Class5/public_subnet.tf
+++ Class5/public_subnet.tf
@@ -20,3 +20,27 @@ resource "aws_subnet" "public3" {
 	map_public_ip_on_launch = true
 	tags = var.tags
 }
+
+resource "aws_route_table" "r" {
+	vpc_id = aws_vpc.main.id
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = aws_internet_gateway.gw.id
+	}
+	tags = var.tags
+}
+
+
+resource "aws_route_table_association" "public1" {
+	subnet_id = aws_subnet.public1.id
+	route_table_id = aws_route_table.r.id
+}
+
+resource "aws_route_table_association" "public2" {
+	subnet_id = aws_subnet.public2.id
+	route_table_id = aws_route_table.r.id
+}
+resource "aws_route_table_association" "public3" {
+	subnet_id = aws_subnet.public3.id
+	route_table_id = aws_route_table.r.id
+}
