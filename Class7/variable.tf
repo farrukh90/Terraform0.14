b631f2daa9e9543212a6467118d91165b67bdf6e
farrukh90 <farrukhsadykov@gmail.com>
2021-02-27 17:01:28 +0000

    added tfvars

--- Class7/variable.tf
+++ Class7/variable.tf
@@ -6,3 +6,30 @@ variable "subnet_ids" {
 variable "db_access" {
   type = list(any)
 }
+variable "identifier" {
+  
+}
+variable "allocated_storage" {
+  
+}
+variable "storage_type" {
+  
+}
+variable "engine" {
+  
+}
+variable "engine_version" {
+  
+}
+variable "instance_class" {
+  
+}
+variable "username" {
+  
+}
+variable "password" {
+  
+}
+variable "publicly_accessible" {
+  
+}
