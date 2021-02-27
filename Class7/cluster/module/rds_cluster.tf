646df161ca2de7b04753d5b9a3953cfc4725f87e
farrukh90 <farrukhsadykov@gmail.com>
2021-02-27 18:52:00 +0000

    added aurora-mysql

--- Class7/cluster/module/rds_cluster.tf
+++ Class7/cluster/module/rds_cluster.tf
@@ -9,7 +9,7 @@ module "db" {
     identifier          = "dbname"
     allocated_storage   = 20
     storage_type        = "gp2"
-    engine              = "mysql"
+    engine              = "aurora-mysql"
     engine_version      = "5.7.mysql_aurora.2.07.2"
     instance_class      = "db.t2.micro"
     username            = "foo"
