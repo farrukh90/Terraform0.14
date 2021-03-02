a15a054d6a7065cf169b08f28ddf1a42cc0d212e
EC2 Default User <ec2-user@ip-172-31-36-95.eu-west-1.compute.internal>
2021-03-02 01:48:15 +0000

    added tfvars

--- Class8/eks.tf
+++ Class8/eks.tf
@@ -11,17 +11,20 @@ provider "kubernetes" {
   # load_config_file       = false
 }
 module "my-cluster" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "12.0.0"
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
-  subnets         = var.subnets
-  vpc_id          = var.vpc_id
+  source                        = "terraform-aws-modules/eks/aws"
+  version                       = "12.0.0"
+  cluster_name                  = var.cluster_name
+  cluster_version               = var.cluster_version
+  subnets                       = var.subnets
+  vpc_id                        = var.vpc_id
+  cluster_create_security_group = true
   worker_groups = [{
     instance_type = var.instance_type
     asg_max_size  = var.asg_max_size
     asg_min_size  = var.asg_min_size #min size should be 1
     }
   ]
+  tags = var.tags
 }
+
 
--- Class8/variable.tf
+++ Class8/variable.tf
@@ -6,9 +6,12 @@ variable "region" {
 variable "cluster_name" {}
 variable "cluster_version" {}
 variable "subnets" {
-    type  = list
+  type = list(any)
 }
 variable "vpc_id" {}
 variable "instance_type" {}
 variable "asg_max_size" {}
 variable "asg_min_size" {}
+variable "tags" {
+  type = map(any)
+}
