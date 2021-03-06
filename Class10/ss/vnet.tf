b04e1d2485ccec5665956c1a31e44b3164f6b328
farrukh90 <farrukhsadykov@gmail.com>
2021-03-06 18:11:43 +0000

    added vnet

---
+++ Class10/ss/vnet.tf
@@ -0,0 +1,7 @@
+
+resource "azurerm_virtual_network" "example" {
+name = "example-network"
+resource_group_name = azurerm_resource_group.example.name
+location = azurerm_resource_group.example.location
+address_space = ["10.0.0.0/16"]
+}
