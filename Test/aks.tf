896d90673b9b627fbfcc8576a4b2726b1699d19b
farrukh90 <farrukhsadykov@gmail.com>
2021-03-10 22:57:04 +0000

    added ["10.1.0.0/24"]

--- Test/aks.tf
+++ Test/aks.tf
@@ -11,8 +11,8 @@ module "network" {
     version = "3.3.0"
   source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.example.name
-  address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.1.0/24"]
+  address_space       = "10.1.0.0/16"
+  subnet_prefixes     = ["10.1.0.0/24"]
   subnet_names        = ["subnet1"]
   depends_on          = [azurerm_resource_group.example]
 }
