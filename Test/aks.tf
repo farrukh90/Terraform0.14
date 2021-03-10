a65607b0b485080e5467bfa3758a8e57195334da
farrukh90 <farrukhsadykov@gmail.com>
2021-03-10 22:51:23 +0000

    added some rnage

--- Test/aks.tf
+++ Test/aks.tf
@@ -11,7 +11,7 @@ module "network" {
     version = "3.3.0"
   source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.example.name
-  address_space       = "10.1.0.0/16"
+  address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.1.0.0/24"]
   subnet_names        = ["subnet1"]
   depends_on          = [azurerm_resource_group.example]
