45a351837ee64d756a20ec8bc895ad01419c8e7d
farrukh90 <farrukhsadykov@gmail.com>
2021-03-11 19:16:11 +0000

    added range

--- Test/filestore/fs.tf
+++ Test/filestore/fs.tf
@@ -7,6 +7,11 @@ resource "google_filestore_instance" "instance" {
     capacity_gb = var.vm_config["capacity_gb"]
     name        = "share1"
   }
+  nfs_export_options {
+      ip_ranges = ["0.0.0.0/0"]
+      access_mode = "READ_WRITE"
+      squash_mode = "NO_ROOT_SQUASH"
+   }
 
   networks {
     network = "default"
--- Test/filestore/variables.tf
+++ Test/filestore/variables.tf
@@ -4,11 +4,6 @@ variable "vm_config" {
     project       = "ckad-302703"
     region        = "us-central1"
     zone          = "us-central1-c"
-    instance_name = "terraform-instance"
-    machine_type  = "f1-micro"
-    image         = "debian-cloud/debian-9"
-    firewall_name = "fw-allow-http"
-    network_tags  = "web"
     filestore_name     = "nfsserver"
     capacity_gb   = 1024
     tier          = "BASIC_HDD"
