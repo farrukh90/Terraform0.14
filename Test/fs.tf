2371cd58519445682fd07e254f48ce10396cca97
farrukh90 <farrukhsadykov@gmail.com>
2021-03-11 18:23:35 +0000

    fixed machine type

--- Test/fs.tf
+++ Test/fs.tf
@@ -1,6 +1,6 @@
 resource "google_filestore_instance" "instance" {
   name = var.vm_config["filestore_name"]
-  zone = var.vm_config["machine_type"]
+  zone = var.vm_config["zone"]
   tier = "PREMIUM"
 
   file_shares {
