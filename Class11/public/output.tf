0e2375c70600a5f41df7c266615d69a02ab0f9f2
farrukh90 <farrukhsadykov@gmail.com>
2021-03-11 02:08:16 +0000

    ran fmt

---
+++ Class11/public/output.tf
@@ -0,0 +1,7 @@
+output "admin_client_certificate" {
+  value = module.aks.admin_client_certificate
+}
+
+output "admin_client_key" {
+  value = module.aks.admin_client_key
+}
--- Class11/public/variable.tf
+++ Class11/public/variable.tf
@@ -1,41 +1,41 @@
 variable "aks_config" {
-	type = map(any)
-	default = {
-		adgroup_name = "AKS-cluster-admins"
-		address_space = "10.1.0.0/16"
-		resource_group_name = "example.name"
-		location = "eastus"
-		client_id = "your-service-principal-client-appid"
-		client_secret = "your-service-principal-client-password"
-		kubernetes_version = "1.19.6"
-		orchestrator_version = "1.19.6"
-		cluster_name = "farrukh" #This should be variablized and changed
-		network_plugin = "azure"
-		os_disk_size_gb = 50
-		sku_tier = "Paid" # defaults to Free
-		enable_role_based_access_control = true
-		rbac_aad_managed = true
-		private_cluster_enabled = true # default value
-		enable_http_application_routing = true
-		enable_azure_policy = true
-		enable_auto_scaling = true
-		agents_min_count = 1
-		agents_max_count = 2
-		agents_count = null # Please set `agents_count` `null` while `enable_auto_scaling` is `true` to avoid possible `agents_count` changes.
-		agents_max_pods = 100
-		agents_pool_name = "exnodepool"
-		agents_availability_zones = "1"
-		agents_type = "VirtualMachineScaleSets"
-	}
+  type = map(any)
+  default = {
+    adgroup_name                     = "AKS-cluster-admins"
+    address_space                    = "10.1.0.0/16"
+    resource_group_name              = "example.name"
+    location                         = "eastus"
+    client_id                        = "your-service-principal-client-appid"
+    client_secret                    = "your-service-principal-client-password"
+    kubernetes_version               = "1.19.6"
+    orchestrator_version             = "1.19.6"
+    cluster_name                     = "farrukh" #This should be variablized and changed
+    network_plugin                   = "azure"
+    os_disk_size_gb                  = 50
+    sku_tier                         = "Paid" # defaults to Free
+    enable_role_based_access_control = true
+    rbac_aad_managed                 = true
+    private_cluster_enabled          = true # default value
+    enable_http_application_routing  = true
+    enable_azure_policy              = true
+    enable_auto_scaling              = true
+    agents_min_count                 = 1
+    agents_max_count                 = 2
+    agents_count                     = null # Please set `agents_count` `null` while `enable_auto_scaling` is `true` to avoid possible `agents_count` changes.
+    agents_max_pods                  = 100
+    agents_pool_name                 = "exnodepool"
+    agents_availability_zones        = "1"
+    agents_type                      = "VirtualMachineScaleSets"
+  }
 }
 
 
 variable "subnet_prefixes" {
-	type = list(any)
+  type = list(any)
 }
 variable "subnet_names" {
-	type = list(any)
+  type = list(any)
 }
 variable "agents_availability_zones" {
-	type = list(any)
+  type = list(any)
 }
