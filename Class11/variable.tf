7488b9b0ad891b9661bff6fddf86e1868eded9f5
farrukh90 <farrukhsadykov@gmail.com>
2021-03-11 01:03:29 +0000

    added variables

--- Class11/variable.tf
+++ Class11/variable.tf
@@ -1,11 +1,18 @@
-variable "client_secret" {
-  type        = string
-  default     = ""
-  description = "description"
-}
-
-variable "client_id" {
-  type        = string
-  default     = ""
-  description = "description"
+variable "aks_config" {
+    type = map 
+    default = {
+        source                       = "farrukh90/aks/azure"
+        cluster_name                 = "example-aks1"
+        kubernetes_version           = "1.18.14"
+        node_pool_name               = "node1"
+        min_count                    = "1"
+        max_count                    = "1"
+        client_id                    = "client_id"
+        client_secret                = "client_secret"
+        environment                  = "dev"
+        resource_group_name_location = "West Europe"
+        resource_group_name          = "dev"
+        username                     = "centos"
+        vm_size                      = "Standard_A2_v2"
+    }
 }
