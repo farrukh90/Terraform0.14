06149a6de2d43456f0014c47faf2e1b511004854
farrukh90 <farrukhsadykov@gmail.com>
2021-03-11 00:36:30 +0000

    added variables

--- Class11/main.tf
+++ Class11/main.tf
@@ -5,8 +5,8 @@ module "aks" {
 	node_pool_name = "node1"
 	min_count = "1"
 	max_count = "1"
-	client_id = ""
-	client_secret = ""
+	client_id = var.client_id
+	client_secret = var.client_secret
 	environment = "dev"
 	resource_group_name_location = "West Europe"
 	resource_group_name = "dev"
---
+++ Class11/variable.tf
@@ -0,0 +1,11 @@
+variable client_secret {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable client_id {
+  type        = string
+  default     = ""
+  description = "description"
+}
