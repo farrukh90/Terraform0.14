7438852c31474f0643ca15ea9f7c723f1854eaab
farrukh90 <farrukhsadykov@gmail.com>
2021-03-11 18:45:04 +0000

    updated fs

--- Test/filestore/fs.tf
+++ Test/filestore/fs.tf
@@ -1,5 +1,4 @@
 resource "google_filestore_instance" "instance" {
-  provider = google-beta
   name = var.vm_config["filestore_name"]
   zone = var.vm_config["zone"]
   tier = var.vm_config["tier"]
@@ -7,16 +6,10 @@ resource "google_filestore_instance" "instance" {
   file_shares {
     capacity_gb = var.vm_config["capacity_gb"]
     name        = "share1"
+  }
 
-    nfs_export_options {
-      ip_ranges = ["0.0.0.0/0"]
-      access_mode = "READ_WRITE"
-      squash_mode = "NO_ROOT_SQUASH"
-   }
-
-#   networks {
-#     network = "default"
-#     modes   = ["MODE_IPV4"]
-#   }
-}
+  networks {
+    network = "default"
+    modes   = ["MODE_IPV4"]
+  }
 }
