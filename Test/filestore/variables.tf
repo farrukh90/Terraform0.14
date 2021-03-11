f545e5bd4ec6390afe9ab1cf519bba99be02925d
farrukh90 <farrukhsadykov@gmail.com>
2021-03-11 18:42:46 +0000

    moved

---
+++ Test/filestore/variables.tf
@@ -0,0 +1,26 @@
+variable "vm_config" {
+  type = map(any)
+  default = {
+    project       = "ckad-302703"
+    region        = "us-central1"
+    zone          = "us-central1-c"
+    instance_name = "terraform-instance"
+    machine_type  = "f1-micro"
+    image         = "debian-cloud/debian-9"
+    firewall_name = "fw-allow-http"
+    network_tags  = "web"
+    filestore_name     = "nfsserver"
+    capacity_gb   = 2660
+    tier          = "BASIC_SSD"
+  }
+}
+variable "labels" {
+  type = map(any)
+  default = {
+    name    = "db"
+    env     = "dev"
+    team    = "devops"
+    quarter = "first"
+    builtby = "me"
+  }
+}
