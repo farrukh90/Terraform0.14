524f4311cdc78064d14b18130a9faf79c38173e1
farrukh90 <farrukhsadykov@gmail.com>
2021-03-11 01:49:29 +0000

    added network version 3.3.0

--- Class11/public/aks.tf
+++ Class11/public/aks.tf
@@ -4,6 +4,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 module "network" {
+    version = "3.3.0"
   source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.example.name
   address_space       = "10.0.0.0/16"
