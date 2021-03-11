b2af5687fefaf3c14f4f3180a1c587934383ac33
farrukh90 <farrukhsadykov@gmail.com>
2021-03-11 00:57:52 +0000

    added some outputs

---
+++ Class11/output.tf
@@ -0,0 +1,39 @@
+output "cluster_id" {
+	value = module.aks.cluster_id
+}
+output "client_certificate" {
+	value = module.aks.client_certificate
+}
+output "kube_config" {
+	value = module.aks.kube_config
+}
+output "cluster_name" {
+	value = module.aks.cluster_name
+}
+output "client_key" {
+	value = module.aks.client_key
+}
+output "client_certificate_output" {
+	value = module.aks.client_certificate_output
+}
+output "cluster_ca_certificate" {
+	value = module.aks.cluster_ca_certificate
+}
+output "host" {
+	value = module.aks.host
+}
+output "username" {
+	value = module.aks.username
+}
+output "password" {
+	value = module.aks.password
+}
+output "raw_kube_config" {
+	value = module.aks.raw_kube_config
+}
+output "location" {
+	value = module.aks.location
+}
+output "kube_config_raw" {
+	value = module.aks.kube_config_raw
+}
