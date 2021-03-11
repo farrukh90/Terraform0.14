f8eaa515823a61343cac972ace73c537af393c09
farrukh90 <farrukhsadykov@gmail.com>
2021-03-11 00:34:33 +0000

    added main.tf

---
+++ Class11/main.tf
@@ -0,0 +1,15 @@
+module "aks" {
+	source = "farrukh90/aks/azure"
+	cluster_name = "example-aks1"
+	kubernetes_version = "1.18.4"
+	node_pool_name = "node1"
+	min_count = "1"
+	max_count = "1"
+	client_id = ""
+	client_secret = ""
+	environment = "dev"
+	resource_group_name_location = "West Europe"
+	resource_group_name = "dev"
+	username = "centos"
+	vm_size = "Standard_A2_v2"
+}
