214f0485b09aa9e0ba052ed0f8a373ae3d8cfc53
farrukh90 <farrukhsadykov@gmail.com>
2021-03-12 20:22:19 +0000

    formatted

--- Test/clouddns/dns.tfvars
+++ Test/clouddns/dns.tfvars
@@ -1,6 +1,6 @@
 config = {
-  project       = "ckad-302703"
-  dns_name = "acirrustech.net."
+  project    = "ckad-302703"
+  dns_name   = "acirrustech.net."
   dns_record = "wordpress"
   rrdatas    = "127.0.0.1"
 }
--- Test/clouddns/variables.tf
+++ Test/clouddns/variables.tf
@@ -1,7 +1,7 @@
 variable "config" {
   type = map(any)
   default = {
-    project       = "ckad-302703"
+    project    = "ckad-302703"
     dns_record = "wordpress"
     dns_name   = "acirrustech.net."
     rrdatas    = "127.0.0.1"
