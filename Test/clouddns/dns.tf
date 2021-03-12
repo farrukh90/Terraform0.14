d2923e80a4415e9c9c6d9265f5e8704eced1765b
farrukh90 <farrukhsadykov@gmail.com>
2021-03-12 20:07:31 +0000

    added wordpress

--- Test/clouddns/dns.tf
+++ Test/clouddns/dns.tf
@@ -1,11 +1,11 @@
 resource "google_dns_record_set" "wordpress" {
-  name         = "wordpress.${google_dns_managed_zone.prod.dns_name}"
+  name         = "wordpress.${google_dns_managed_zone.wordpress.dns_name}"
   type         = "A"
   ttl          = 60
-  managed_zone = google_dns_managed_zone.prod.name
+  managed_zone = google_dns_managed_zone.wordpress.name
   rrdatas      = ["127.0.0.1"]
 }
-resource "google_dns_managed_zone" "prod" {
+resource "google_dns_managed_zone" "wordpress" {
   name     = "wordpress"
   dns_name = "acirrustech.net."
 }
