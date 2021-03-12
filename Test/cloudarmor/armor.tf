f6bc61e11c112f2c38fb3864b27bbf0eeaae2f92
farrukh90 <farrukhsadykov@gmail.com>
2021-03-12 21:56:11 +0000

    fixed

--- Test/cloudarmor/armor.tf
+++ Test/cloudarmor/armor.tf
@@ -16,7 +16,7 @@ resource "google_compute_security_policy" "policy" {
     priority = "1001"
     match {
       expr {
-        expression = "origin.region_code == "${var.china}" && origin.region_code == "RU" && origin.region_code == "UA""
+        expression = "origin.region_code == '${var.china}' && origin.region_code == "RU" && origin.region_code == "UA""
       }
     }
     description = "Blacklisted Countries"
