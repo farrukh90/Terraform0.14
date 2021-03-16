b16d7881f587b9e3254caee41696d9d7748f88cc
farrukh90 <farrukhsadykov@gmail.com>
2021-03-16 15:32:43 +0000

    added s3

--- Test/bucket/bucket.tf
+++ Test/bucket/bucket.tf
@@ -10,10 +10,23 @@ provider "google" {
   project = var.bucket_config["project"]
   region  = var.bucket_config["region"]
 }
-
-resource "google_storage_bucket" "bucket" {
-  name          = var.bucket_config["bucket_name"]
-  force_destroy = true
-  
+resource "google_storage_bucket" "default" {
+  name          = var.bucket_config["name"]
+  location      = var.bucket_config["region"]
+  storage_class = var.storage_class
+  force_destroy = var.force_destroy
+  lifecycle_rule {
+    action {
+      type          = var.action_type
+      storage_class = var.action_storage_class
+    }
+    condition {
+      age                   = var.age
+      created_before        = var.created_before
+      with_state            = var.with_state
+      matches_storage_class = var.matches_storage_class
+      num_newer_versions    = var.num_newer_versions
+    }
+  }
 }
 
