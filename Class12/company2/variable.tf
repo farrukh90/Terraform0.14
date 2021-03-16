3a37696c1e408e41ffd7ae13e9ce3ed482240b6c
farrukh90 <farrukhsadykov@gmail.com>
2021-03-16 00:40:18 +0000

    added output and variables

---
+++ Class12/company2/variable.tf
@@ -0,0 +1,11 @@
+variable "config" {
+  type = map(any)
+  default = {
+    environment       = "dev"
+    s3_bucket         = "backend-state-farrukh"  #Will be used to set backend.tf
+    s3_folder_project = "application"            #Will be used to set backend.tf
+    s3_folder_region  = "us-east-1"              #Will be used to set backend.tf
+    s3_folder_type    = "sharedtools"            #Will be used to set backend.tf
+    s3_tfstate_file   = "infrastructure.tfstate" #Will be used to set backend.tf
+  }
+}
