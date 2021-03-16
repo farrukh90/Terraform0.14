96f375b32240052a215790ca4e496d8ea23f547e
farrukh90 <farrukhsadykov@gmail.com>
2021-03-16 15:26:17 +0000

    added l policy

--- Test/bucket/bucket.tf
+++ Test/bucket/bucket.tf
@@ -1,25 +1,30 @@
 variable "bucket_config" {
-        type = map(any)
-        default = {
-                project = "ckad-302703"
-                bucket_name = "bucket-terraform-farrukh"
-                region = "us-central1"
-                zone = "us-central1-c"
-                bucket_location = "US"
-        }
+  type = map(any)
+  default = {
+    project         = "ckad-302703"
+    bucket_name     = "bucket-terraform-farrukh"
+    region          = "us-central1"
+    zone            = "us-central1-c"
+    bucket_location = "US"
+  }
 }
 provider "google" {
-        project = var.bucket_config["project"]
-        region = var.bucket_config["region"]
-        zone = var.bucket_config["zone"]
+  project = var.bucket_config["project"]
+  region  = var.bucket_config["region"]
+  zone    = var.bucket_config["zone"]
 }
 
 resource "google_storage_bucket" "bucket" {
-        name = var.bucket_config["bucket_name"]
-        force_destroy = true
-        storage_class = "REGIONAL"
-        action = {
-                type = "SetStorageClass"
-        }
+  name          = var.bucket_config["bucket_name"]
+  force_destroy = true
+  storage_class = "REGIONAL"
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
 }
 
