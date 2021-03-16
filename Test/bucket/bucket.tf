be27591a6c33ee460aacadbcdf07099d2500cbbc
farrukh90 <farrukhsadykov@gmail.com>
2021-03-16 15:20:52 +0000

    added bucket

---
+++ Test/bucket/bucket.tf
@@ -0,0 +1,22 @@
+variable "bucket_config" {
+        type = map(any)
+        default = {
+                project = "ckad-302703"
+                bucket_name = "bucket-terraform-farrukh"
+                region = "us-central1"
+                zone = "us-central1-c"
+                bucket_location = "US"
+        }
+}
+provider "google" {
+        project = var.bucket_config["project"]
+        region = var.bucket_config["region"]
+        zone = var.bucket_config["zone"]
+}
+
+resource "google_storage_bucket" "bucket" {
+        name = var.bucket_config["bucket_name"]
+        location = var.bucket_config["bucket_location"]
+        force_destroy = true
+}
+
