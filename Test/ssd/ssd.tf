f893063c760c1072df6fdc1ec3f3438d596055d2
farrukh90 <farrukhsadykov@gmail.com>
2021-03-16 14:45:22 +0000

    added 5G

--- Test/ssd/ssd.tf
+++ Test/ssd/ssd.tf
@@ -2,7 +2,7 @@ resource "google_compute_disk" "default" {
   name  = "test-disk"
   type  = "pd-ssd"
   zone  = "us-central1-a"
-  size = 1024
+  size = 5
   labels = {
     environment = "dev"
   }
