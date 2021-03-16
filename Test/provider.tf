9b9dc6efe9398e2aa51c3c226b1ef3a612cb0786
farrukh90 <farrukhsadykov@gmail.com>
2021-03-16 14:17:55 +0000

    added variables

--- Test/provider.tf
+++ Test/provider.tf
@@ -1,3 +1,5 @@
 provider "google" {
-	project = var.config["project"]
+    project = var.config["project"]
+	region = var.config["region"]
+	zone = var.config["zone"]
 }
--- Test/variable.tf
+++ Test/variable.tf
@@ -2,5 +2,7 @@ variable "config" {
 	type = map(any)
 	default = {
 		project = "cache-307814"
+		region = "us-central1"
+		zone = "us-central1-c"
 	}
 }
