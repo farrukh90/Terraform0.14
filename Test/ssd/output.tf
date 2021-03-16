80fba7268277864748772b8e6495061b4f689294
farrukh90 <farrukhsadykov@gmail.com>
2021-03-16 14:52:28 +0000

    added output

--- Test/ssd/output.tf
+++ Test/ssd/output.tf
@@ -1,9 +1,7 @@
-# output config {
-#   value       = [
-#     google_redis_instance.cache.current_location_id,
-#     google_redis_instance.cache.host,
-#     google_redis_instance.cache.port,
-#     google_redis_instance.cache.id
-#   ]
-# }
+output config {
+  value       = [
+    google_compute_disk.default.id,
+    google_compute_disk.default.creation_timestamp,
+  ]
+}
 
