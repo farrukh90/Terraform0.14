f3c5a177cc15087550da2755712e51d2b4bd75cd
farrukh90 <farrukhsadykov@gmail.com>
2021-03-16 00:53:45 +0000

    added variable

--- Class12/company2/variable.tf
+++ Class12/company2/variable.tf
@@ -7,5 +7,6 @@ variable "config" {
     s3_folder_region  = "us-east-1"              #Will be used to set backend.tf
     s3_folder_type    = "sharedtools"            #Will be used to set backend.tf
     s3_tfstate_file   = "infrastructure.tfstate" #Will be used to set backend.tf
+    region        = "us-east-1"
   }
 }
