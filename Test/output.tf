dc3f7b2e1a530860777b79bf8a771ccff607f597
farrukh90 <farrukhsadykov@gmail.com>
2021-03-16 14:32:11 +0000

    added id

--- Test/output.tf
+++ Test/output.tf
@@ -1,4 +1,8 @@
 output config {
-  value       = google_redis_instance.cache.current_location_id
+  value       = 
+    google_redis_instance.cache.current_location_id,
+    google_redis_instance.cache.host,
+    google_redis_instance.cache.port,
+    google_redis_instance.cache.id
 }
 
